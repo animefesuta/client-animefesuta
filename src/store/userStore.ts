708dc4468747dadb5760a7c12f2481ae292d9686
baizhi958216 <1475289190@qq.com>
2024-02-05 13:26:30 +0800

    feat: 路由更换为更新的storage
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface UserState {
   userloginstate: boolean;
@@ -20,7 +20,7 @@ export const useUserStore = create(
     }),
     {
       name: "userStore",
-      getStorage: () => localStorage,
+      storage: createJSONStorage(() => localStorage),
     }
   )
 );
