9e973163c9207275128bc645399e331583586fd4
baizhi958216 <1475289190@qq.com>
2024-03-14 18:07:24 +0800

    feat: 路由拦截
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,7 +5,24 @@ import NoPage from "@/pages/NoPage";
 import Pics from "@/pages/Pics";
 import Posts from "@/pages/Posts";
 import User from "@/pages/User";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
+
+/**
+ * @description 路由权限控制
+ * @description (取代请求拦截器中的登陆重定向)
+ */
+const rootLoader = async () => {
+  const userStore = localStorage.getItem("userStore");
+  if (userStore) {
+    const { userInfo, userloginstate, userToken } = JSON.parse(userStore).state;
+    if (userInfo && userloginstate && userToken) {
+      return { userInfo, userloginstate, userToken };
+    }
+    return redirect("/");
+  } else {
+    return redirect("/");
+  }
+};
 
 export const router = createBrowserRouter([
   {
@@ -17,14 +34,17 @@ export const router = createBrowserRouter([
       {
         path: "/picback",
         element: <Pics />,
+        loader: rootLoader,
       },
       {
         path: "/live",
         element: <Live />,
+        loader: rootLoader,
       },
       {
         path: "/posts",
         element: <Posts />,
+        loader: rootLoader,
       },
       {
         path: "/user",
@@ -39,6 +59,7 @@ export const router = createBrowserRouter([
             element: <User />,
           },
         ],
+        loader: rootLoader,
       },
     ],
   },
