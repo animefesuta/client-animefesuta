e8ec94154a55fe2fdade98dbdff8df16cc10dbca
baizhi958216 <1475289190@qq.com>
2024-03-19 23:24:45 +0800

    feat: 配置vite axios接口
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "@/plugins/axios";
 
 type UserForm = {
   username: string;
@@ -8,7 +8,7 @@ type UserForm = {
 };
 
 const signin = async (data: UserForm) => {
-  const res = await axios.post("http://localhost:8080/api/v1/auth/signup", {
+  const res = await axios.post("/api/v1/auth/signup", {
     nickname: data.username,
     email: data.useremail,
     password: data.password,
@@ -17,7 +17,7 @@ const signin = async (data: UserForm) => {
 };
 
 const login = async (data: Pick<UserForm, "useremail" | "password">) => {
-  const res = await axios.post("http://localhost:8080/api/v1/auth/login", {
+  const res = await axios.post("/api/v1/auth/login", {
     email: data.useremail,
     password: data.password,
   });
@@ -25,12 +25,9 @@ const login = async (data: Pick<UserForm, "useremail" | "password">) => {
 };
 
 const getUserInfo = async (email: string) => {
-  const res = await axios.post(
-    "http://localhost:8080/api/v1/fesuta/user/findUserByEmail",
-    {
-      email: email,
-    }
-  );
+  const res = await axios.post("/api/v1/fesuta/user/findUserByEmail", {
+    email: email,
+  });
   return res.data;
 };
 
