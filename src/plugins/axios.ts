2eaa6227c3b863aff4f328760ad1951a94fd6bbe
baizhi958216 <1475289190@qq.com>
2024-03-20 16:14:32 +0800

    feat: axios请求头加入token
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
 
-export default axios.create({
+// 创建一个axios实例，用于登录和注册请求
+const loginRegisterInstance = axios.create({
   baseURL: import.meta.env.VITE_API_ENDPOINT,
 });
+
+loginRegisterInstance.interceptors.request.use((config) => {
+  if (config.url?.includes("/login") || config.url?.includes("signup")) {
+    return config;
+  }
+  config.headers.Authorization = `Bearer ${
+    JSON.parse(localStorage.getItem("userStore") ?? "").state.userToken
+  }`;
+  return config;
+});
+export default loginRegisterInstance;
