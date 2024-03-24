8b9f895182e07623e2ee62bae203abe0b7ac7823
baizhi958216 <1475289190@qq.com>
2024-03-24 15:59:49 +0800

    fix: 返图卡片标题溢出
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/pages/Pics/_components/BlankCard.tsx
+++ src/pages/Pics/_components/BlankCard.tsx
@@ -1,3 +1,9 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import clsx from "clsx";
 
 interface CardProps {
@@ -15,8 +21,30 @@ const BlankCard: React.FC<CardProps> = ({ ...CardProps }) => {
         className={clsx("rounded-lg w-full h-[239px] bg-cover bg-center")}
       ></div>
       <div>
-        <div className="text-[18px]">{CardProps.cardTitle}</div>
-        <div className="text-[13px] text-[#999]">{CardProps.cardType}</div>
+        <div className="text-[18px]">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="w-full truncate text-start">
+                {CardProps.cardTitle}
+              </TooltipTrigger>
+              <TooltipContent align="start">
+                <p>{CardProps.cardTitle}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+        <div className="text-[13px] text-[#999]">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="text-start w-full truncate">
+                {CardProps.cardType}
+              </TooltipTrigger>
+              <TooltipContent align="start">
+                <p>{CardProps.cardType}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
       </div>
     </div>
   );
